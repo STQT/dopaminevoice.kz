02e5f02367c6eceeaea3d6193816f896fbbecd24
Kairat Sultan <garri.sultonov@mail.ru>
2022-11-09 15:13:00 +0600

    production changes database configs

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -83,12 +83,9 @@ if DEBUG:
         }
     }
 else:
-    DATABASES = {
-        "default": os.getenv(
-            "DATABASE_URL",
-            default="postgres://postgres/postgres",
-        ),
-    }
+    DATABASES["default"] = env.db("DATABASE_URL")  # noqa F405
+    DATABASES["default"]["ATOMIC_REQUESTS"] = True  # noqa F405
+    DATABASES["default"]["CONN_MAX_AGE"] = env.int("CONN_MAX_AGE", default=60)  # noqa F405
 
     LOGGING = {
         "version": 1,
